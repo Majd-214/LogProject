71cd1d05f47dfdc295ec2dcbde84cd1edf69c014
Majd Aburas <1aburasmaj@hdsb.ca>
2023-05-08 14:38:34 -0400

    Fixed a whole lot of stuff

--- HandTracker.py
+++ HandTracker.py
@@ -25,6 +25,12 @@ screen_width, screen_height = get_monitors()[0].width, get_monitors()[0].height
 # Define the crop offset of the projection in camera pixels
 crop_offset = 80
 
+# initialize sensitivity value
+sensitivity = 20
+
+# initialize previous touch state
+previous_touch_state = False
+
 # Initialize webcam feeds individually
 capture = cv2.VideoCapture(camera_index)
 capture.set(3, cam_width)
@@ -49,18 +55,36 @@ mod = np.float32([[crop_offset, crop_offset], [cam_width - crop_offset, crop_off
 # Retrieve projection corner data from YAML file
 with open('dist/RUNTIME DATA/Resources/Corners.yaml', 'r') as openfile:
     # Reading from YAML file
-    corners = yaml.load(openfile, Loader=yaml.FullLoader)
+    corners_file = yaml.load(openfile, Loader=yaml.FullLoader)
 
-contour = np.float32([(corners['Top Left']['X'], corners['Top Left']['Y']),
-                      (corners['Top Right']['X'], corners['Top Right']['Y']),
-                      (corners['Bottom Left']['X'], corners['Bottom Left']['Y']),
-                      (corners['Bottom Right']['X'], corners['Bottom Right']['Y'])])
+corners = np.float32([(corners_file['Top Left']['X'], corners_file['Top Left']['Y']),
+                      (corners_file['Top Right']['X'], corners_file['Top Right']['Y']),
+                      (corners_file['Bottom Left']['X'], corners_file['Bottom Left']['Y']),
+                      (corners_file['Bottom Right']['X'], corners_file['Bottom Right']['Y'])])
 
 # Calculate perspective transformation matrices from projection corners
-tracking_matrix = cv2.getPerspectiveTransform(contour, dst)
+tracking_matrix = cv2.getPerspectiveTransform(corners, dst)
 
 # Initialize hand tracker
 detector = Htm.HandDetector(max_hands=1, detection_con=0.3, track_con=0.4)
+
+
+def get_touch_sensitivity(image, px, py):
+    sensitivity_region_size = 20
+    y_min = int(max(0, py - sensitivity_region_size / 2))
+    y_max = int(min(image.shape[0], py + sensitivity_region_size / 2))
+    x_min = int(max(0, px - sensitivity_region_size / 2))
+    x_max = int(min(image.shape[1], px + sensitivity_region_size / 2))
+    cropped = image[y_min:y_max, x_min:x_max]
+    if cropped.size == 0:
+        return 0.0
+    else:
+        mean_value = np.mean(cropped)
+        if mean_value == 0:
+            return 0.0
+        else:
+            return 1 / mean_value
+
 
 while True:
     # Read webcam feeds
@@ -81,10 +105,23 @@ while True:
             y = np.interp(transformed[0][0][1], (0, cam_height), (0, screen_height))
             z = landmarks[8][3]
             mouse.move(int(x), int(y))
-            if detector.fingers_up()[0] == 0:
-                mouse.click()
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Apply thresholding to obtain a binary image
+            thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)[1]
+
+            # Find contours in the binary image
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Define a sensitivity threshold
+            sensitivity = 127
+
+            # Loop over all contours and check if any have a point with a value greater than the sensitivity threshold
+            print('Touch Sensitivity: ' + str(get_touch_sensitivity(gray, int(x), int(y))))
+
         for x in range(0, 4):
-            cv2.circle(img, (int(contour[x][0]), int(contour[x][1])), 1, (0, 255, 0), cv2.FILLED)
+            cv2.circle(img, (int(corners[x][0]), int(corners[x][1])), 1, (0, 255, 0), cv2.FILLED)
 
         # final_img = cv2.warpPerspective(img, tracking_matrix, (cam_width, cam_height))
         cv2.imshow('Webcam Feed', img)
